9ad3dd9d3175ae9ab721821b718c52ba34ef7993
Alexandr Timofeev <a.timoff3@gmail.com>
2026-03-31 23:43:21 +0300

    K2: Add pydantic model HealthResponse to represent schema of /api/health endpoint response.
    It's good way and practice to code with FastAPI and pydantic.

--- backend/src/app/api/health.py
+++ backend/src/app/api/health.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
+from app.schemas.health import HealthResponse
+
 router = APIRouter()
 
-@router.get("/health")
-def health_check():
-    return {
-        "status": "ok"
-    }
+@router.get("/health", response_model=HealthResponse)
+def health_check() -> HealthResponse:
+    return HealthResponse(
+        status="ok"
+    )
