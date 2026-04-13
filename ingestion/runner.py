7cb3ca86614d0f674f8cd5192a5f2b11888ac581
Alexandr Timofeev <a.timoff3@gmail.com>
2026-04-13 16:00:46 +0300

    K9: Follow up 3bd4377. Add deduplicator class as parameter into runner.py:IngestionRunner

--- ingestion/runner.py
+++ ingestion/runner.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from deduplicator import Deduplicator
 from publisher.base import Publisher
 from sources.base import Source
 from transformers.base import Transformer
@@ -10,10 +11,18 @@ class IngestionRunner:
     source: Source
     transformer: Transformer
     publisher: Publisher
+    deduplicator: Deduplicator | None = None
 
     def run_once(self) -> list:
         raw_data = self.source.fetch()
         measurements = self.transformer.transform(raw_data)
+
+        if self.deduplicator is not None:
+            measurements = [
+                measurement
+                for measurement in measurements
+                if self.deduplicator.is_new(measurement)
+            ]
 
         if measurements:
             self.publisher.publish(measurements)
