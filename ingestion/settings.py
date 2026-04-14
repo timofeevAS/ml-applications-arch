d273bde8aaed852b26ac817a5ecce1e1b4337ead
Alexandr Timofeev <a.timoff3@gmail.com>
2026-04-14 17:35:32 +0300

    K9: Add aliases for fields from .env in settings.py
    This provide more easily-supported code.

--- ingestion/settings.py
+++ ingestion/settings.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    polling_seconds_timeout: int = Field(default=10, ge=1)
-    digitraffic_stations_ids: list[int] = Field(default_factory=list)
+    polling_seconds_timeout: int = Field(default=10, ge=1, alias="POLLING_SECONDS_TIMEOUT")
+    digitraffic_stations_ids: list[int] = Field(default_factory=list, alias="DIGITRAFFIC_STATIONS_IDS")
 
     model_config = SettingsConfigDict(
         env_file=".env",
