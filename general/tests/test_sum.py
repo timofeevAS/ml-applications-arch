22c2283399d2b30be564e6c2ee6cf2ccfdf62eb5
Julskl <klimashova.yu@gmail.com>
2026-04-15 19:45:28 +0300

    Merge branch for task K5: Add CI workflow
    * Create sum.py
    
    K5-2: sum.py: Аn example of a function to check the test in yml
    
    * Create test_sum.py
    
    K5-2: test_sum.py: An example of tests for a function
    
    * Create sample.yml
    
    K5-2: sample.yml: An example with lint and test
    
    * Update sample.yml
    
    K5-2: sample.yml: removed hard-coded branch list, updated pytest command

---
+++ general/tests/test_sum.py
@@ -0,0 +1,10 @@
+from general.sum import sum
+
+def test_positive_numbers():
+    assert sum(1, 1) == 2
+
+def test_zero():
+    assert sum(5, 0) == 5
+
+def test_negative_numbers():
+    assert sum(-1, -2) == -3
