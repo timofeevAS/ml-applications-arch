8645304c0e071ce36cd37522559165cc41d1aace
obrezgina <obrezgina@mail.ru>
2026-05-04 14:59:54 +0300

    On K12: обновлено описание полей SensorResponse

--- backend/app/schemas/sensors.py
+++ backend/app/schemas/sensors.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 
-'''
-id      — номер датчика
-name    — название для интерфейса
-metric  — колонка из CSV, которую этот датчик измеряет
-unit    — единица измерения
-'''
 
 class SensorResponse(BaseModel):
+    """
+    id      номер датчика
+    name    название для интерфейса
+    metric  колонка из CSV, которую этот датчик измеряет
+    unit    единица измерения
+    """
     id: int
     name: str
     metric: str
